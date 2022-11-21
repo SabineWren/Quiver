e64fd4172c32d9524d78175cbfee48212bdc1fab
SabineWren <sabinewrensc@gmail.com>
2022-11-21 01:35:34 -0800

    Another migration fix

--- Migrations/M001.lua
+++ Migrations/M001.lua
@@ -3,14 +3,14 @@ Quiver_Migrations_M001 = function()
 	if mstore == nil or Quiver_Store.FrameMeta == nil then return end
 
 	-- Rename swing timer module
-	if mstore["AutoShotCastbar"] then
-		mstore[Quiver_Module_AutoShotTimer.Id] = mstore["AutoShotCastbar"]
-		mstore["AutoShotCastbar"] = nil
-	end
-	if Quiver_Store.FrameMeta["AutoShotCastbar"] then
-		Quiver_Store.FrameMeta[Quiver_Module_AutoShotTimer.Id] = Quiver_Store.FrameMeta["AutoShotCastbar"]
-		Quiver_Store.FrameMeta["AutoShotCastbar"] = nil
-	end
+	Quiver_Store.ModuleEnabled[Quiver_Module_AutoShotTimer.Id] = Quiver_Store.ModuleEnabled["AutoShotCastbar"]
+	Quiver_Store.ModuleEnabled["AutoShotCastbar"] = nil
+
+	mstore[Quiver_Module_AutoShotTimer.Id] = mstore["AutoShotCastbar"]
+	mstore["AutoShotCastbar"] = nil
+
+	Quiver_Store.FrameMeta[Quiver_Module_AutoShotTimer.Id] = Quiver_Store.FrameMeta["AutoShotCastbar"]
+	Quiver_Store.FrameMeta["AutoShotCastbar"] = nil
 
 	-- Move all module-specific frame data into module stores
 	for _k, v in _G.Quiver_Modules do
