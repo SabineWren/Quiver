9da6cdf69b0d3e12c57a3471f4a996b1453ac706
SabineWren <sabinewrensc@gmail.com>
2023-01-08 07:12:35 -0800

    Removed some accidental copy-paste code

--- Migrations/M003.lua
+++ Migrations/M003.lua
@@ -2,12 +2,6 @@ Quiver_Migrations_M003 = function()
 	local mstore = Quiver_Store.ModuleStore or {}
 	local s = mstore[Quiver_Module_TranqAnnouncer.Id] or {}
 
-	-- Change colour to color
-	if s.ColourShoot then s.ColorShoot = s.ColourShoot end
-	if s.ColorReload then s.ColorReload = s.ColourReload end
-	s.ColourShoot = nil
-	s.ColourReload = nil
-
 	if s.MsgTranqHit then
 		-- We notify on tranq cast instead of hit. To prevent a breaking
 		-- release version, attempt changing contradictory text.
