293b013fb635fb6a7a28bc0e8b8e42b4bc2348a3
SabineWren <sabinewrensc@gmail.com>
2024-09-09 03:08:15 -0700

    Added working FdPrepareTrap function

--- MacroFunctions.lua
+++ MacroFunctions.lua
@@ -20,25 +20,31 @@ local CastPetAction = function(actionName)
 	end
 end
 
---- Casts feign death (if needed) and sets pet passive (if needed)
---- Usage:
---- /cast "Frost Trap"<br>
---- /script Quiver.FdForTrap("Frost Trap")
----@param trapName string
----@return nil
-local FdForTrap = function(trapName)
-	local spellIndex = Spell.FindSpellIndex(trapName)
+-- Casts feign death (if needed) and sets pet passive (if needed).
+-- Usage:
+-- /cast Frost Trap
+-- /script Quiver.FdPrepareTrap()
+local FdPrepareTrap = function()
+	-- Requires level 16, which makes it the lowest level trap
+	local trap = Quiver.L.Spell["Immolation Trap"]
+	local spellIndex = Spell.FindSpellIndex(trap)
 	if spellIndex == nil then
-		DEFAULT_CHAT_FRAME:AddMessage("Could not find "..trapName.." in spellbook.")
+		DEFAULT_CHAT_FRAME:AddMessage("Could not find "..trap.." in spellbook.")
 	else
-		local timeStartCd, _ = GetSpellCooldown(spellIndex, BOOKTYPE_SPELL)
-		if timeStartCd == 0 and UnitAffectingCombat("player") then
+		local timeStartCdTrap, _ = GetSpellCooldown(spellIndex, BOOKTYPE_SPELL)
+		local timeStartCdFd = 0
+
+		local fd = Quiver.L.Spell["Feign Death"]
+		local fdIndex = Spell.FindSpellIndex(fd)
+		if fdIndex ~= nil then
+			timeStartCdFd = GetSpellCooldown(fdIndex, BOOKTYPE_SPELL)
+		end
+
+		if timeStartCdTrap == 0 and timeStartCdFd == 0 and UnitAffectingCombat("player") then
 			if UnitExists("pettarget") and UnitAffectingCombat("pet") then
 				PetPassiveMode()
 			end
-			-- TODO implement more locale strings
-			local fd = Quiver.L["Feign Death"]
-			CastSpellByName("Feign Death")
+			CastSpellByName(fd)
 		end
 	end
 end
@@ -46,6 +52,6 @@ end
 return function()
 	Quiver.CastNoClip = CastNoClip
 	Quiver.CastPetAction = CastPetAction
-	Quiver.FdForTrap = FdForTrap
+	Quiver.FdPrepareTrap = FdPrepareTrap
 	Quiver.PredMidShot = AutoShotTimer.PredMidShot
 end
